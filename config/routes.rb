0d3047086f8b9384f5f3a1666c473887dbcf26a7
Rachel Moser <moser.rachel@gmail.com>
2021-01-11 00:13:48 -0600

    Update root path and nav links

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'events#index'
   resources :users, only: [:show]
   resources :events, only: [:index, :new, :create, :show]
 end
