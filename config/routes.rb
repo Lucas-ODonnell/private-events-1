89410c963fa5c4443535cbea27f44e2e529c859b
Rachel Moser <moser.rachel@gmail.com>
2021-01-14 12:34:54 -0600

    Add users#show as authenticated_root

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@
 
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end
+  
   root 'events#index'
+
   resources :users, only: [:show]
   resources :events, only: [:index, :new, :create, :show, :edit, :update]
 end
