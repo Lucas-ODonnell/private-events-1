bfc882ecbf2af12c96dc5721b4da55ea5d2ec461
Rachel Moser <moser.rachel@gmail.com>
2021-01-18 15:03:01 -0600

    Update edit method to add new attendees

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,59 +13,64 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
     # This tells the form to display the invitation section
-    # IS THIS LINE CAUSING AN ISSUE???
     @event.invitations.build
     # Remove the current user from this list?
     @users = User.all
-    # @user_options = User.all.map{ |u| [ u.name, u.id ] }
   end
   
   def create
-
-    # WTF IS THIS LINE DOING!!!
     @event = current_user.created_events.build(event_params)
-    # @event = Event.new(event_params)
-    # @event.creator = current_user
-    # @event.invitations.build(event_params)
-    # @event.invitations.build(attendee_id: 'id')
     
     if @event.save
-      flash[:notice] = "Your event was created!"
+      invitation_params[:invitations][:attendee_id].each do |attendee_id|
+        @event.invitations.create(attendee_id: attendee_id)
+      end
+
+      flash[:notice] = "Your event and invitations were created!"
       redirect_to @event
     else
-      # @user_options = User.all.map{ |u| [ u.name, u.id ] }
       @users = User.all
-      # byebug
-      # raise 'hell'
       render :new
     end
   end
 
   def edit
     @event = Event.find(params[:id])
+    # Remove the current user from this list?
+    @users = User.all
   end
 
   def update
     @event = Event.find(params[:id])
 
     if @event.update(event_params)
-      flash[:notice] = "Your event was updated!"
+      # Need to remove/destroy invitations if un-checked
+      invitation_params[:invitations][:attendee_id].each do |attendee_id|
+        next if @event.attendees.ids.include?(attendee_id.to_i)
+
+        @event.invitations.create(attendee_id: attendee_id)
+      end
+      flash[:notice] = "Your event and invitations were updated!"
       redirect_to @event
     else
       render :edit
+      @users = User.all
     end
   end
 
   private
   
     def event_params
-      params.require(:event).permit(:creator_id, 
+      params.require(:event).permit(:creator_id,
                                     :title,
-                                    :start_time, 
-                                    :end_time, 
-                                    :location, 
-                                    :description,
-                                    invitations_attributes: [:attendee_id => []])
+                                    :start_time,
+                                    :end_time,
+                                    :location,
+                                    :description)
+    end
+
+    def invitation_params
+      params.require(:event).permit(invitations: [:attendee_id => []])
     end
 
     def correct_creator
